0635ef0cb5ba5e2cb0c83eb7614d9d46c777d4a0
Ben Ford <ben@itlogic.com.au>
2018-04-13 17:06:11 +1000

    Half main code complete

--- Autologin.ps1
+++ Autologin.ps1
@@ -1,1 +1,19 @@
-.\wfreerdp.exe /?
+# Import CSV
+$CSV = Import-CSV Logins.csv
+
+# Cycle Through each entry
+ForEach ($User in $CSV) {
+
+# Set some easy variables
+    $Username = $User.Username
+    $Password = $User.Password
+    $Hostname = $User.Password
+
+    # Actually run the RDP - Note wfreerdp provides no output on success or error.
+    .\wfreerdp.exe /v:$Hostname /u:$Username /p:$Password
+
+    # Wait 5 seconds for good measure
+    Start-Sleep -Seconds 5
+    # Need to do the confirm if worked and continue sections.
+
+}
